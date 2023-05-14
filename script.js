54f4321e4c8a3d2f83a8f5edc7772ffcc8504a79
Vadim <vadimyatsunyk@gmail.com>
2023-05-14 23:29:15 +0200

    2329

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+
+// ========menu
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
