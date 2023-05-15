9c98fdeb7543bc825953a0667c943d78094ec15b
Danil Krasnov <99325740+Hellblazer98@users.noreply.github.com>
2023-05-15 15:40:17 +0300

    finish

--- script.js
+++ script.js
@@ -16,5 +16,6 @@ for (i = 0; i < acc.length; i++) {
 function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
   document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
